1599cde2e09c6655b40e53be1c6bb35f94a037df
Naman123457 <carry69yt@gmail.com>
2025-07-03 11:21:37 +0000

    type conversion done

---
+++ 01_basics/type_Conversion.js
@@ -0,0 +1,13 @@
+let a=33
+//console.log(typeof a)
+let stringNumber=String(a)
+//console.log(typeof stringNumber)
+let b="33abc"
+//console.log(typeof b)
+let c=Number(b)
+//console.log(c)
+//console.log(typeof c)
+let d=false
+console.log(Number(d))
+let e=""
+console.log(Boolean(e))
