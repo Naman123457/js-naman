5d854d8c1375ca35e093a56dd1f07845b8f2ed75
Naman123457 <carry69yt@gmail.com>
2025-07-03 15:16:21 +0000

    addition of call by refrence and value concept

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -1,5 +1,7 @@
 //  Primitive
 
+//const { useInsertionEffect } = require("react");
+
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
 const score = 100
@@ -35,3 +37,23 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+let myCourse="javascript from basic"
+let myCourse2=myCourse
+myCourse2="i am naman"
+console.log(myCourse)
+console.log(myCourse2)
+let useOne={
+    email:"carry69yt@gmail.com",
+    name:"naman"
+}
+let useTwo=useOne
+useTwo.email="naman.kumar0418@gmail.com"
+console.log(useOne.email)
+console.log(useTwo.email)
+/*
+useOne is an object (non-primitive).
+
+useTwo = useOne means both variables reference the same object in memory.
+
+So, when you modify useTwo.email, it also changes useOne.email — because they both point to the same object
+*/
