83b6ae894012db1afa69c5e9fb41ddb0c0ae832c
Naman123457 <carry69yt@gmail.com>
2025-07-03 11:31:52 +0000

    fully conversion with contenation

--- 01_basics/type_Conversion.js
+++ 01_basics/type_Conversion.js
@@ -1,13 +1,66 @@
-let a=33
-//console.log(typeof a)
-let stringNumber=String(a)
-//console.log(typeof stringNumber)
-let b="33abc"
-//console.log(typeof b)
-let c=Number(b)
-//console.log(c)
-//console.log(typeof c)
-let d=false
-console.log(Number(d))
-let e=""
-console.log(Boolean(e))
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+ console.log(negValue);
+
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2**3);
+ //console.log(2/3);
+ //console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+ console.log("1" + 2);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
+
+ console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+ console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
