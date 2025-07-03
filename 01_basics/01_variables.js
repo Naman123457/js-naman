7fa347add9e37e4bb46350f0df1703950ac1a57a
Naman123457 <carry69yt@gmail.com>
2025-07-03 08:21:25 +0000

    Added variable file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=13445
+let accountEmail="carry69yt@gmail.com"
+var accountPassword="naman@124"
+accountCity="Patna"
+let accountState;
+
+//accountId=123443 not allowed
+accountEmail="nasbjds@gmail.com"
+//accountPassword="12345"
+accountCity="tamil"
+/*
+prefer not to use var
+*/
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
